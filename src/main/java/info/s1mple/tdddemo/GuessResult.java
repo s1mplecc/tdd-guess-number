bc21cb67a62f7d8a6da5821d8ba89238e0233581
s1mple <798342418@qq.com>
2018-08-15 10:46:41 +0800

    #tdd-demo - s1mple - 重构通过 actualAnswer 和 inputAnswer 计算并组装猜测结果的方法

--- src/main/java/info/s1mple/tdddemo/GuessResult.java
+++ src/main/java/info/s1mple/tdddemo/GuessResult.java
@@ -2,7 +2,6 @@ package info.s1mple.tdddemo;
 
 import com.google.common.collect.Lists;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class GuessResult {
@@ -26,31 +25,21 @@ public class GuessResult {
     }
 
     public void analyzeBy(final Answer actualAnswer, final Answer inputAnswer) {
-        ArrayList<Integer> actualAnswerNumbers = Lists.newArrayList(actualAnswer.answerNumbers());
-        ArrayList<Integer> inputAnswerNumbers = Lists.newArrayList(inputAnswer.answerNumbers());
-
         this.inputAnswer = inputAnswer;
-        this.result = computeACountBy(actualAnswerNumbers, inputAnswerNumbers) + "A"
-                + computeBCountBy(actualAnswerNumbers, inputAnswerNumbers) + "B";
+        this.result = computeAndComposeResultBy(actualAnswer, inputAnswer);
+        // todo: change correct status -> true if 4A
     }
 
-    private int computeACountBy(List<Integer> actualAnswerNumbers, List<Integer> inputAnswerNumbers) {
-        int aCount = 0;
-        for (int i = actualAnswerNumbers.size() - 1; i >= 0; i--) {
-            if (actualAnswerNumbers.get(i).equals(inputAnswerNumbers.get(i))) {
-                aCount++;
-                actualAnswerNumbers.remove(i);
-                inputAnswerNumbers.remove(i);
-            }
-        }
-        if (aCount == 4) {
-            this.correct = true;
-        }
-//        actualAnswerNumbers.stream().filter(x -> x.equals(inputAnswerNumbers.get(actualAnswerNumbers.indexOf(x)))).count();
-        return aCount;
-    }
+    private String computeAndComposeResultBy(final Answer actualAnswer, final Answer inputAnswer) {
+        List<Integer> inputAnswerNumbers = Lists.newArrayList(inputAnswer.answerNumbers());
+        List<Integer> actualAnswerNumbers = Lists.newArrayList(actualAnswer.answerNumbers());
 
-    private int computeBCountBy(List<Integer> actualAnswerNumbers, List<Integer> inputAnswerNumbers) {
-        return (int) actualAnswerNumbers.stream().filter(inputAnswerNumbers::contains).count();
+        int aCount = (int) actualAnswerNumbers.stream()
+                .filter(x -> x.equals(inputAnswerNumbers.get(actualAnswerNumbers.indexOf(x))))
+                .count();
+        int bCount = (int) actualAnswerNumbers.stream()
+                .filter(inputAnswerNumbers::contains)
+                .count() - aCount;
+        return aCount + "A" + bCount + "B";
     }
 }
